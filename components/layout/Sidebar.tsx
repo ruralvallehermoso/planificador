5ae398b01df839f77b4e4720bc1539664febc5e8
ruralvallehermoso <ruralvallehermoso@gmail.com>
2026-02-05 10:23:04 +0100

    chore: remove vault integration from sidebar

--- components/layout/Sidebar.tsx
+++ components/layout/Sidebar.tsx
@@ -39,7 +39,7 @@ const allNavigation: NavItem[] = [
         children: [
             { name: 'Portfolio Master', href: '/finanzas/portfolio', icon: TrendingUp },
             { name: 'Simulador Financiero', href: '/finanzas/simulador', icon: BarChart3 },
-            { name: 'Vault Seguro', href: '/finanzas/vault', icon: Shield },
+            { name: 'Simulador Financiero', href: '/finanzas/simulador', icon: BarChart3 },
         ]
     },
     {
